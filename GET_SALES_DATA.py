b00b09e1f95fcb16c68b21e2d02f71b9fcdc948d
svlipatov <svlipatov@bk.ru>
2024-05-01 12:09:36 +0300

    lab1

--- GET_SALES_DATA.py
+++ GET_SALES_DATA.py
@@ -1,28 +1,22 @@
 import psycopg2
 import pandas as pd
-# Пароль брать из локального файла
-# Соединение
-with psycopg2.connect(dbname='chinook', user='postgres',
-                        password='Postgres!1', host='localhost') as conn:
-    sql = "select to_char(i.invoice_date,'YYYYMM') as month, al.artist_id as artist, genre_id as genre, \
-                        media_type_id as format, cu.country, sum(il.quantity * il.unit_price) as amount from \
-                        invoice_line as il \
-                        inner join \
-                        invoice  as i \
-                        on il.invoice_id = i.invoice_id \
-                        left join \
-                        track as tr \
-                        on il.track_id = tr.track_id \
-                        left join \
-                        album as al \
-                        on tr.album_id = al.album_id \
-                        left join \
-                        customer as cu \
-                        on i.customer_id = cu.customer_id \
-                        group by \
-                        month, artist, genre, \
-                        format, country"
-    # Результат запроса в DataFrame
-    df_sales = pd.read_sql(sql, conn)
-print(df_sales)
+def get_sales_data_f():
+    # Импорт параметров БД
+    df_db = pd.read_csv('/Users/sergei/PycharmProjects/ML_2023_1/DB/db.csv')
+
+    # Соединение
+    with psycopg2.connect(dbname=df_db.loc[0,'dbname'], user=df_db.loc[0,'user'],
+                            password=df_db.loc[0,'password'], host=df_db.loc[0,'host']) as conn:
+        sql = "SELECT date, sum(sales) as sales FROM public.\"SALES\" group by date"
+        # Результат запроса в DataFrame
+        df_sales = pd.read_sql(sql, conn)
+        return df_sales
+
+df_sales_data = get_sales_data_f()
+if __name__ == "__main__":
+    print(df_sales_data)
+
+
+
+
 
