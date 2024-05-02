0d219b2851c38a1078642c914a6bb6d51eaffcf2
svlipatov <svlipatov@bk.ru>
2024-05-02 21:26:44 +0300

    py files changes

--- LAB1/GET_SALES_DATA.py
+++ LAB1/GET_SALES_DATA.py
@@ -1,11 +1,17 @@
 import psycopg2
 import pandas as pd
+import socket
 def get_sales_data_f():
     # Импорт параметров БД
-    df_db = pd.read_csv('db.csv')
+    df_db = pd.read_csv('db.csv',delimiter=';')
+    # При обращение к хосту из контейнера имя хоста указывается вдругом виде
+    if socket.gethostname()[-5:] == 'local':
+        host = df_db.loc[0,'host']
+    # jenkins
+    else: host = df_db.loc[0,'host2']
     # Соединение
     with psycopg2.connect(dbname=df_db.loc[0,'dbname'], user=df_db.loc[0,'user'],
-                            password=df_db.loc[0,'password'], host=df_db.loc[0,'jenkins host']) as conn:
+                            password=df_db.loc[0,'password'], host=host) as conn:
         sql = "SELECT date, sum(sales) as sales FROM public.\"SALES\" group by date"
         # Результат запроса в DataFrame
         df_sales = pd.read_sql(sql, conn)
